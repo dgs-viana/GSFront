5d9a91ddcfa2b943abb2d97bd70200a55d510b02
Felipe Bonilha <felpsbonilha@gmail.com>
2025-06-05 21:34:56 -0300

    Commit final

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-const imagens = [
-  "img/primeira-imagem.jpg",
-  "img/segunda-imagem.jpg",
-  "img/terceira-imagem.jpg"
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const imagens = [
+    "img/segunda-imagem.jpg",
+    "img/primeira-imagem.jpg",
+    "img/terceira-imagem.jpg"
+  ];
 
+  let index = 0;
+  const carousel = document.getElementById("carousel-img");
 
-let index = 0;
-const carousel = document.getElementById("carousel-img");
+  function trocarImagem() {
+    index = (index + 1) % imagens.length;
+    carousel.src = imagens[index];
+  }
 
+  
 
-function trocarImagem() {
-  index = (index + 1) % imagens.length;
-  carousel.src = imagens[index];
-}
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
 
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
 
-setInterval(trocarImagem, 5000);
-
-
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+  document.querySelectorAll('.color-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      const newColor = btn.getAttribute('data-color');
+      document.documentElement.style.setProperty('--cor-fundo1', newColor);
+      document.documentElement.style.setProperty('--cor-div-tecnologias', newColor);
+      document.querySelector('nav').style.backgroundColor = newColor;
+      document.querySelector('footer').style.backgroundColor = newColor;
+    });
+  });
 });
-
-
-
